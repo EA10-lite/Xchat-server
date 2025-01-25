9cac3e41b53a6ac0440732131a477f0cf393f5cb
Emmanuel <emanuelanyigor@gmail.com>
2025-01-25 05:23:36 +0100

    initialzing socket

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const cors = require('cors');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = app.listen(4000, () => {
+    console.log('Server is running on port 3000');
+});
+
+// static files
+
+app.use(express.static('public'));
+
+app.use(
+    cors({
+      origin: ["http://localhost:3000", "http://localhost:3001"],
+      credentials: true,
+    })
+);
+  
+const io = new Server(server, {
+    cors: {
+      origin: 'http://localhost:3000',
+      methods:['GET','POST']
+    }
+})
+
+// Socket setup 
+// const io = socket(server);
+io.on('connection', (socket) => {
+    socket.on('chat', (data) => {
+        console.log(data);
+        io.sockets.emit('chat', data);
+    });
+});
